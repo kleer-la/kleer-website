200ea4edc431a41ac87ce22b9af53bf953ec1790
Carlos Peix <carlos.peix@gmail.com>
2012-12-15 22:40:13 -0200

    Unifique el uso de tabs en la identacion de codigo y markup. Borre un archivo de step definitions

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,19 +8,19 @@ require 'rack/test'
 require 'webrat'
 
 Webrat.configure do |config|
-  config.mode = :rack
+	config.mode = :rack
 end
 
 class MyWorld
-  include Rack::Test::Methods
-  include Webrat::Methods
-  include Webrat::Matchers
+	include Rack::Test::Methods
+	include Webrat::Methods
+	include Webrat::Matchers
 
-  Webrat::Methods.delegate_to_session :response_code, :response_body
+	Webrat::Methods.delegate_to_session :response_code, :response_body
 
-  def app
-    Sinatra::Application
-  end
+	def app
+		Sinatra::Application
+	end
 end
 
 World{MyWorld.new}
