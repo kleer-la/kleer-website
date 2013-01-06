ba8fa459aee92ac329ca3978e0827b4eef8378af
Carlos Peix <carlos.peix@gmail.com>
2013-01-06 11:42:12 -0300

    Mensaje 'Cargando...' mientras se cargan los eventos por ajax.

--- public/js/dt_cursos_home.js
+++ public/js/dt_cursos_home.js
@@ -6,7 +6,6 @@ var oTable;
 $(document).ready(function() {
 	oTable = $('#cursos').dataTable( {
 		"bLengthChange": false,
-		/*"bProcessing": true, lo anule porque ocupa mucho lugar encima de la tabla*/
 		"sAjaxSource": '/entrenamos/eventos/proximos',
 		"aoColumns": [
 			{ "sWidth": "5%" },
@@ -21,7 +20,7 @@ $(document).ready(function() {
 		"oLanguage": {
 			"sProcessing":   "Procesando...",
 			"sLengthMenu":   "Mostrar _MENU_ registros",
-			"sZeroRecords":  "<div class=\"alert alert-warning\">No tenemos cursos en los próximos dos meses pero nos gustaría que nos contactes a <a href=\"mailto:hola@kleer.la\">hola@kleer.la</a> con tu inquietud.</div>",
+			"sZeroRecords":  "<div class=\"alert alert-warning center\">Cargando...</div>",
 			"sInfo":         "",
 			"sInfoEmpty":    "",
 			"sInfoFiltered": "",
