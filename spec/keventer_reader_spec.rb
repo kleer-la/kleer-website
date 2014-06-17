293cd8d942bdd60241ffb407de197b14dc98afaf
Juan Gabardini <jgabardini@computer.org>
2014-06-17 14:22:19 -0300

    refactor to_boolean, test it and fixit for '' string; refactor on create_event

--- spec/keventer_reader_spec.rb
+++ spec/keventer_reader_spec.rb
@@ -3,6 +3,29 @@ require File.join(File.dirname(__FILE__),'../lib/keventer_reader')
 require 'date'
 require 'spec_helper'
 
+describe "to_boolean" do
+  it "Should convert string to true" do
+    to_boolean("true").should be true
+    to_boolean("True").should be true
+    to_boolean("t").should be true
+    to_boolean("yes").should be true
+    to_boolean("y").should be true
+    to_boolean("1").should be true
+  end
+  it "Should convert string to false" do
+    to_boolean("false").should be false
+    to_boolean("f").should be false
+    to_boolean("no").should be false
+    to_boolean("n").should be false
+    to_boolean("").should be false
+    to_boolean("0").should be false
+    to_boolean(nil).should be false
+  end
+  it "should raise ArgumentError for arguments that are not boolean" do
+    expect{ to_boolean("pepe") }.to raise_error(ArgumentError)
+  end
+end
+
 describe KeventerReader do
   
   it "Should Be Able to Load an Xml File for Events" do
