0030516367184a922ea55290341a215d560812a2
Juan Gabardini <jgabardini@computer.org>
2015-12-17 10:04:54 -0300

    refactoring: moving xml loading to EventType class

--- lib/keventer_event_type.rb
+++ lib/keventer_event_type.rb
@@ -1,12 +1,13 @@
 class KeventerEventType
-  attr_accessor :id, :name, :goal, :description, :recipients, :program, :duration, :faqs, 
-                :elevator_pitch, :learnings, :takeaways, :elevator_pitch, :include_in_catalog, 
+  attr_accessor :id, :name, :subtitle, :goal, :description, :recipients, :program, :duration, :faqs,
+                :elevator_pitch, :learnings, :takeaways, :elevator_pitch, :include_in_catalog,
                 :public_editions, :average_rating, :net_promoter_score, :surveyed_count,
                 :promoter_count
-  
+
   def initialize
     @id = nil
     @name = ""
+    @subtitle= ""
     @goal = ""
     @description = ""
     @recipients = ""
@@ -24,12 +25,40 @@ class KeventerEventType
     @surveyed_count = 0
     @promoter_count = 0
   end
-  
+
   def uri_path
     @id.to_s + "-" + @name.downcase.gsub(/ /, "-")
   end
-  
+
   def has_rate
     surveyed_count > 20 && !average_rating.nil?
   end
+
+  def load_string(xml, field)
+    element= xml.find_first(field.to_s)
+    if not element.nil?
+      send(field.to_s+"=", element.content)
+    end
+  end
+
+  def load(xml_keventer_event)
+    @id  = xml_keventer_event.find_first('id').content.to_i
+    @duration = xml_keventer_event.find_first('duration').content.to_i
+
+    [:name, :subtitle, :description, :learnings, :takeaways,
+      :goal, :recipients, :program].each {
+      |f| load_string(xml_keventer_event, f)
+    }
+
+    @faqs  = xml_keventer_event.find_first('faq').content
+    @elevator_pitch = xml_keventer_event.find_first('elevator-pitch').content
+    @include_in_catalog = to_boolean( xml_keventer_event.find_first('include-in-catalog').content )
+
+    @average_rating = xml_keventer_event.find_first('average-rating').content.nil? ? nil : xml_keventer_event.find_first('average-rating').content.to_f.round(2)
+    @net_promoter_score = xml_keventer_event.find_first('net-promoter-score').content.nil? ? nil : xml_keventer_event.find_first('net-promoter-score').content.to_i
+    @surveyed_count = xml_keventer_event.find_first('surveyed-count').content.to_i
+    @promoter_count = xml_keventer_event.find_first('promoter-count').content.to_i
+  end
+
+
 end
--- spec/keventer_event_type_spec.rb
+++ spec/keventer_event_type_spec.rb
@@ -1,85 +1,147 @@
+# encoding: utf-8
+
 require File.join(File.dirname(__FILE__),'../lib/keventer_event_type')
 require 'spec_helper'
 
 describe KeventerEventType do
-  
+
   before(:each) do
-    @keventype = KeventerEventType.new
+    @keventtype = KeventerEventType.new
   end
-  
+
   it "should have a name" do
-    @keventype.name = "Workshop de Retrospectivas"
-    @keventype.name.should == "Workshop de Retrospectivas"
+    @keventtype.name = "Workshop de Retrospectivas"
+    @keventtype.name.should == "Workshop de Retrospectivas"
   end
-  
+
   it "should have a goal" do
-    @keventype.goal = "Buenos Aires"
-    @keventype.goal.should == "Buenos Aires"
+    @keventtype.goal = "Buenos Aires"
+    @keventtype.goal.should == "Buenos Aires"
   end
-  
+
   it "should have a description" do
-    @keventype.description = "Argentina"
-    @keventype.description.should == "Argentina"
+    @keventtype.description = "Argentina"
+    @keventtype.description.should == "Argentina"
   end
-  
+
   it "should have a elevator_pitch" do
-    @keventype.elevator_pitch = "Argentina"
-    @keventype.elevator_pitch.should == "Argentina"
+    @keventtype.elevator_pitch = "Argentina"
+    @keventtype.elevator_pitch.should == "Argentina"
   end
 
   it "should have rating instance variable" do
-    @keventype.average_rating.should == 0.0
-    @keventype.net_promoter_score.should == 0
-    @keventype.surveyed_count.should == 0.0
-    @keventype.promoter_count.should == 0
+    @keventtype.average_rating.should == 0.0
+    @keventtype.net_promoter_score.should == 0
+    @keventtype.surveyed_count.should == 0.0
+    @keventtype.promoter_count.should == 0
   end
-  
+
   it "new event_type doesn't have rate" do
-    @keventype.has_rate.should be_false
+    @keventtype.has_rate.should be_false
   end
 
   it "should have rate" do
-    @keventype.average_rating = 3
-    @keventype.surveyed_count= 100
-    @keventype.has_rate.should be_true
+    @keventtype.average_rating = 3
+    @keventtype.surveyed_count= 100
+    @keventtype.has_rate.should be_true
   end
 
   it "empty event_type doesn't have rate" do
-    @keventype.average_rating = nil
-    @keventype.surveyed_count= 100
-    @keventype.has_rate.should be_false
+    @keventtype.average_rating = nil
+    @keventtype.surveyed_count= 100
+    @keventtype.has_rate.should be_false
   end
 
 
   it "should have a learnings" do
-    @keventype.learnings = "Argentina"
-    @keventype.learnings.should == "Argentina"
+    @keventtype.learnings = "Argentina"
+    @keventtype.learnings.should == "Argentina"
   end
-  
+
   it "should have a takeaways" do
-    @keventype.takeaways = "Argentina"
-    @keventype.takeaways.should == "Argentina"
+    @keventtype.takeaways = "Argentina"
+    @keventtype.takeaways.should == "Argentina"
   end
-  
+
   it "should have a program" do
-    @keventype.program = "ar"
-    @keventype.program.should == "ar"
+    @keventtype.program = "ar"
+    @keventtype.program.should == "ar"
   end
-  
+
   it "should have a duration" do
-    @keventype.duration = 16
-    @keventype.duration.should == 16
+    @keventtype.duration = 16
+    @keventtype.duration.should == 16
   end
-  
+
   it "should have some recipients" do
-    @keventype.recipients = "sdkjfhskjfhskdjf"
-    @keventype.recipients.should == "sdkjfhskjfhskdjf"
+    @keventtype.recipients = "sdkjfhskjfhskdjf"
+    @keventtype.recipients.should == "sdkjfhskjfhskdjf"
   end
-  
+
   it "should have some FAQs" do
-    @keventype.faqs = "sdkjfhskjfhskdjf"
-    @keventype.faqs.should == "sdkjfhskjfhskdjf"
-  end  
-  
-  
+    @keventtype.faqs = "sdkjfhskjfhskdjf"
+    @keventtype.faqs.should == "sdkjfhskjfhskdjf"
+  end
+
+  it "should have an empty subtitle on creatio" do
+    expect(@keventtype.subtitle).to eq("")
+  end
+
+  context "Loding from xml" do
+    def build_and_parse(xml_element)
+      initial = %(<?xml version="1.0" encoding="UTF-8"?>
+      <event-type>
+        <average-rating type="decimal" nil="true"/>
+        <created-at type="datetime">2014-09-27T17:34:01Z</created-at>
+        <csd-eligible type="boolean">false</csd-eligible>
+        <description>de tres dias</description>
+        <duration type="integer">24</duration>
+        <elevator-pitch>de tres dias</elevator-pitch>
+        <faq></faq>
+        <goal></goal>
+        <id type="integer">5</id>
+        <include-in-catalog type="boolean">false</include-in-catalog>
+        <learnings></learnings>
+        <materials></materials>
+        <name>Curso de 3 días</name>
+        <net-promoter-score type="integer" nil="true"/>
+        <program>de tres dias</program>
+        <promoter-count type="integer" nil="true"/>
+        <recipients>de tres dias</recipients>
+        <surveyed-count type="integer" nil="true"/>
+        <tag-name></tag-name>
+        <takeaways></takeaways>
+        <updated-at type="datetime">2014-09-27T17:34:01Z</updated-at>
+      )
+      closing= "</event-type>"
+      parser =  LibXML::XML::Parser.string( initial + xml_element + closing )
+      doc = parser.parse
+      doc.find('/event_type')
+      doc
+    end
+
+
+    it "should load basic info: id, description & duration" do
+      @keventtype.load build_and_parse("")
+
+      expect(@keventtype.id).to eq(5)
+      expect(@keventtype.name).to eq("Curso de 3 días")
+      expect(@keventtype.duration).to eq(24)
+    end
+
+    it "should load an emtpy subtitle" do
+      @keventtype.load build_and_parse('<subtitle type="string" nil="true"/>')
+
+      expect(@keventtype.subtitle).to eq("")
+    end
+
+    it "should load a subtitle" do
+      @keventtype.load build_and_parse('<subtitle>Good to see you!</subtitle>')
+
+      expect(@keventtype.subtitle).to eq("Good to see you!")
+    end
+
+
+  end
+
 end
