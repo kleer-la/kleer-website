5b0b90f2f06ea3909d44d999e2def1a3b5b2e939
Juan Gabardini <jgabardini@computer.org>
2015-12-19 20:46:44 -0300

    subtitle in event type detail page

--- features/step_definitions/event_type_steps.rb
+++ features/step_definitions/event_type_steps.rb
@@ -7,16 +7,17 @@ def stub_connector( test_file = "just_one_event.xml")
   connector.stub(:community_events_xml_url).and_return( File.join(File.dirname(__FILE__),"../../spec/community_events.xml") )
   connector.stub(:kleerers_xml_url).and_return( File.join(File.dirname(__FILE__),"../../spec/kleerers.xml") )
   connector.stub(:categories_xml_url).and_return( File.join(File.dirname(__FILE__),"../../spec/categories.xml") )
-  
+
 end
 
 def get_event_type(event_type_id)
   connector = double("KeventerConnector")
-  connector.stub(:events_xml_url).and_return( File.join(File.dirname(__FILE__),"../../spec/events.xml") )
-  connector.should_receive(:event_type_url).with(event_type_id) #TODO: move to RSpec
+  connector.should_receive(:event_type_url).with(event_type_id)
   connector.stub(:event_type_url).and_return( File.join(File.dirname(__FILE__),"../../spec/event_type_#{event_type_id}.xml") )
+  connector.stub(:events_xml_url).and_return( File.join(File.dirname(__FILE__),"../../spec/events.xml") )
   connector.stub(:categories_xml_url).and_return( File.join(File.dirname(__FILE__),"../../spec/categories.xml") )
-  @@keventer_reader.connector = connector    
+  @@keventer_reader.connector = connector
+
 end
 
 Given(/^theres an event type$/) do
@@ -40,7 +41,7 @@ When(/^I visit a non existing event type page$/) do
   connector.should_receive(:event_type_url)
   connector.stub(:event_type_url).and_return( nil)
   connector.stub(:categories_xml_url).and_return( File.join(File.dirname(__FILE__),"../../spec/categories.xml") )
-  @@keventer_reader.connector = connector    
+  @@keventer_reader.connector = connector
 
   visit '/categoria/productos-robustos/cursos/1-xxx'
 end
@@ -55,4 +56,13 @@ end
 
 Then(/^I should not see a rating$/) do
   expect(last_response.body).not_to have_selector('.stars')
+end
+
+Given(/^there is a event type with subtitle$/) do
+  @event_type_id=1
+  get_event_type(@event_type_id)
+end
+
+When(/^I visit this event type page$/) do
+  visit "/categoria/productos-robustos/cursos/#{@event_type_id}-xxx"
 end
